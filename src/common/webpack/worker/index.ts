3d249ae421a49756f12f1a5910977de28539221c
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2023-11-17 13:22:56 +0100

    refactor: extract `SharedWebWorker`

---
+++ src/common/webpack/worker/index.ts
@@ -0,0 +1,2 @@
+export {SharedWebWorker as SharedWorker} from './shared-web-worker';
+export {WebWorker as Worker} from './web-worker';
--- src/common/webpack/worker/shared-web-worker.ts
+++ src/common/webpack/worker/shared-web-worker.ts
@@ -1,7 +1,7 @@
 import {generateWorkerLoader} from './generate-worker-loader';
 
-export class WebWorker extends Worker {
-    constructor(url: string | URL, options?: WorkerOptions) {
+export class SharedWebWorker extends SharedWorker {
+    constructor(url: string | URL, options?: string | WorkerOptions) {
         const objectURL = generateWorkerLoader(url);
         super(objectURL, options);
         URL.revokeObjectURL(objectURL);
