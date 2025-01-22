a68347c96dcf688344f5b4a886c434ed5dcf9e76
Valerii Sidorenko <balepas@nebius.com>
2025-01-22 22:54:55 +0100

    fix(babel-preset): remove @babel/plugin-transform-class-properties from preset
    @babel/preset-env fixes Safari 15 bug with @babel/plugin-bugfix-safari-class-field-initializer-scope.

--- src/common/babel/index.ts
+++ src/common/babel/index.ts
@@ -7,7 +7,7 @@ export function babelPreset(config: {newJsxTransform?: boolean; isSsr?: boolean}
                 bugfixes: true,
                 targets: config.isSsr ? {node: 'current'} : undefined,
             },
-            runtime: {version: '^7.13.10'},
+            runtime: {version: '^7.26.0'},
             typescript: true,
             react: {
                 runtime: config.newJsxTransform ? 'automatic' : 'classic',
