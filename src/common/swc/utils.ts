4e388cbb475ed935b6bd1ee9dd4c5215387c2e12
dgaponov <dgaponov@lenta.ru>
2025-09-08 15:55:39 +0300

    feat: additional paths for SWC server compilation (#264)

--- src/common/swc/utils.ts
+++ src/common/swc/utils.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import {convert} from 'tsconfig-to-swcconfig';
 
+const DEFAULT_EXCLUDE = ['node_modules'];
+
 export const EXTENSIONS_TO_COMPILE = ['.js', '.ts', '.mts', '.mjs', '.cjs'];
 
 function resolvePaths(paths: Record<string, string[]>, baseUrl: string) {
@@ -14,17 +16,41 @@ function resolvePaths(paths: Record<string, string[]>, baseUrl: string) {
     return entries;
 }
 
-export function getSwcOptionsFromTsconfig(projectPath: string, filename = 'tsconfig.json') {
+export type GetSwcOptionsFromTsconfigOptions = {
+    projectPath: string;
+    filename?: string;
+    additionalPaths?: string[];
+    exclude?: string | string[];
+};
+
+export function getSwcOptionsFromTsconfig({
+    projectPath,
+    filename = 'tsconfig.json',
+    additionalPaths,
+    exclude,
+}: GetSwcOptionsFromTsconfigOptions) {
     const swcOptions = convert(filename, projectPath);
+    swcOptions.exclude = swcOptions.exclude || [];
     swcOptions.jsc = {
         ...swcOptions.jsc,
         // SWC requires absolute path as baseUrl
         baseUrl: projectPath,
     };
 
+    let customExclude: string[] = [];
+    if (Array.isArray(exclude)) {
+        customExclude = exclude;
+    } else if (exclude) {
+        customExclude = [exclude];
+    }
+
+    swcOptions.exclude = [...(swcOptions.exclude || []), ...DEFAULT_EXCLUDE, ...customExclude];
+
     // SWC don't compile referenced files like tsc, so we need collect all directories to compile.
     const paths = swcOptions.jsc.paths || {};
-    const directoriesToCompile = [projectPath, ...resolvePaths(paths, projectPath)];
+    const directoriesToCompile = [
+        ...new Set([projectPath, ...resolvePaths(paths, projectPath), ...(additionalPaths || [])]),
+    ];
 
     return {
         swcOptions,
